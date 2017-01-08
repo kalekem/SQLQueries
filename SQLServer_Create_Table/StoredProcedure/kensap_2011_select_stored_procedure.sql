da98fa47df6cb8f7c914b06eac4705401f85a4fc
Cleophas Kalekem <kalekemcleophas01@gmail.com>
2017-01-07 21:18:09 -0800

    Added more stored and exec stored procedures

---
+++ SQLServer_Create_Table/StoredProcedure/kensap_2011_select_stored_procedure.sql
@@ -0,0 +1,21 @@
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE dbo.kensap_class_of_2011 
+
+AS
+BEGIN
+	
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM kensap_2011;
+END
+GO
---
+++ SQLServer_Create_Table/StoredProcedure/kensap_2016_select_stored_procedure.sql
@@ -0,0 +1,22 @@
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE dbo.kensap_class_of_2016
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM kensap_2016;
+END
+GO
